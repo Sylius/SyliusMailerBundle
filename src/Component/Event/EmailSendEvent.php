e3f1042caf1e7f408e1b992742630124be30fa93
Grzegorz Sadowski <sadowskigp@gmail.com>
2020-01-31 17:33:55 +0100

    Add SyliusLabs/PolyfillSymfonyEventDispatcher

--- src/Component/Event/EmailSendEvent.php
+++ src/Component/Event/EmailSendEvent.php
@@ -14,7 +14,7 @@ declare(strict_types=1);
 namespace Sylius\Component\Mailer\Event;
 
 use Sylius\Component\Mailer\Model\EmailInterface;
-use Symfony\Contracts\EventDispatcher\Event;
+use SyliusLabs\Polyfill\Symfony\EventDispatcher\Event;
 
 final class EmailSendEvent extends Event
 {
