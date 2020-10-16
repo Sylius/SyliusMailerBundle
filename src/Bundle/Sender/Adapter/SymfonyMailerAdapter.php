fb7ada61909c71c9728845616cea80bca9364e80
Youri Westerman <youri.westerman@nedac.eu>
2020-10-16 14:02:28 +0200

    Added symfony mailer adapter

---
+++ src/Bundle/Sender/Adapter/SymfonyMailerAdapter.php
@@ -0,0 +1,82 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Sylius\Bundle\MailerBundle\Sender\Adapter;
+
+use Sylius\Bundle\MailerBundle\Exception\SyliusMailerException;
+use Sylius\Component\Mailer\Event\EmailSendEvent;
+use Sylius\Component\Mailer\Model\EmailInterface;
+use Sylius\Component\Mailer\Renderer\RenderedEmail;
+use Sylius\Component\Mailer\Sender\Adapter\AbstractAdapter;
+use Sylius\Component\Mailer\SyliusMailerEvents;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
+use Symfony\Component\Mime\Email;
+use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
+
+final class SymfonyMailerAdapter extends AbstractAdapter
+{
+    /** @var MailerInterface */
+    private $mailer;
+
+    public function __construct(MailerInterface $mailer, ?EventDispatcherInterface $dispatcher = null)
+    {
+        parent::__construct($dispatcher);
+
+        $this->mailer = $mailer;
+    }
+
+    public function send(
+        array $recipients,
+        string $senderAddress,
+        string $senderName,
+        RenderedEmail $renderedEmail,
+        EmailInterface $email,
+        array $data,
+        array $attachments = [],
+        array $replyTo = []
+    ): void {
+        $message = (new Email())
+            ->subject($renderedEmail->getSubject())
+            ->from(new Address($senderAddress, $senderName))
+            ->to($recipients)
+            ->replyTo($replyTo)
+            ->html($renderedEmail->getBody())
+        ;
+
+        foreach ($attachments as $attachment) {
+            $message->attachFromPath($attachment);
+        }
+
+        $emailSendEvent = new EmailSendEvent($message, $email, $data, $recipients, $replyTo);
+
+        if ($this->dispatcher !== null) {
+            $this->dispatcher->dispatch($emailSendEvent, SyliusMailerEvents::EMAIL_PRE_SEND);
+        }
+
+        try {
+            $this->mailer->send($message);
+        } catch (TransportExceptionInterface $transportException) {
+            throw new SyliusMailerException(
+                'Failed to send email message: ' . $transportException->getMessage(),
+                0,
+                $transportException
+            );
+        }
+
+        if ($this->dispatcher !== null) {
+            $this->dispatcher->dispatch($emailSendEvent, SyliusMailerEvents::EMAIL_POST_SEND);
+        }
+    }
+}
