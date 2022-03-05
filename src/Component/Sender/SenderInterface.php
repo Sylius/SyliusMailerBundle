21f56600e0c884def72e5311da743830e2a21d55
Michael Babker <michael.babker@gmail.com>
2022-03-05 13:08:44 -0600

    Document array structures

--- src/Component/Sender/SenderInterface.php
+++ src/Component/Sender/SenderInterface.php
@@ -15,5 +15,10 @@ namespace Sylius\Component\Mailer\Sender;
 
 interface SenderInterface
 {
+    /**
+     * @param string[] $recipients A list of email addresses to receive the message.
+     * @param string[] $attachments A list of file paths to attach to the message.
+     * @param string[] $replyTo A list of email addresses to set as the Reply-To address for the message.
+     */
     public function send(string $code, array $recipients, array $data = [], array $attachments = [], array $replyTo = []): void;
 }
