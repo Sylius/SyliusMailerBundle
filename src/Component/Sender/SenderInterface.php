7c8c2f703589a3e1181336bcb17c2a196c4ae35a
Łukasz Chruściel <lchrusciel@gmail.com>
2022-04-26 13:58:23 +0200

    [Sender] Assert recipient emails are always not empty strings

--- src/Component/Sender/SenderInterface.php
+++ src/Component/Sender/SenderInterface.php
@@ -16,7 +16,7 @@ namespace Sylius\Component\Mailer\Sender;
 interface SenderInterface
 {
     /**
-     * @param string[] $recipients A list of email addresses to receive the message.
+     * @param string[]|null[] $recipients A list of email addresses to receive the message. Providing null or empty string in the list of recipients is deprecated and will be removed in SyliusMailerBundle 2.0
      * @param string[] $attachments A list of file paths to attach to the message.
      * @param string[] $replyTo A list of email addresses to set as the Reply-To address for the message.
      */
